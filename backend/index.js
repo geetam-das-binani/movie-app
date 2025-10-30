725a498daed399f90f112999547e95fba319ac13
geetam <geetambinani6@gmail.com>
2025-10-31 00:19:48 +0530

    added deployment scripts

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
-
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config({ path: "./.env" });
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 8001;
 const app = express();
 app.use(
@@ -18,6 +20,8 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
